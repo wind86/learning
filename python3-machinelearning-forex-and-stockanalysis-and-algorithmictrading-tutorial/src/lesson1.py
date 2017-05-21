f37662efd6978d079c0ee2f64ca0ecf4594689de
wind86 <wind86@meta.ua>
2017-05-22 00:58:28 +0300

    start following tutorial

---
+++ python3-machinelearning-forex-and-stockanalysis-and-algorithmictrading-tutorial/src/lesson1.py
@@ -0,0 +1,10 @@
+'''
+Created on May 22, 2017
+
+Machine Learning and Pattern Recognition for Algorithmic Forex and Stock Trading: Intro 
+based on https://www.youtube.com/watch?v=v_L9jR8P-54&index=1&list=PLQVvvaa0QuDe6ZBtkCNWNUbdaBo2vA4RO
+
+@author: ubuntu
+'''
+import matplotlib
+import numpy
